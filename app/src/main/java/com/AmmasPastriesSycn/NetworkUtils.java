fe06363e562b6f9100ec1d83b3fa036a36f2e1c9
Syed Abdul Muqeeth <samx123786@gmail.com>
2023-09-23 22:02:14 +0530

    Digital-Signage-Sync

--- app/src/main/java/com/AmmasPastriesSycn/NetworkUtils.java
+++ app/src/main/java/com/AmmasPastriesSycn/NetworkUtils.java
@@ -6,19 +6,11 @@ import android.net.NetworkInfo;
 
 public class NetworkUtils {
     public static boolean isNetworkAvailable(Context context) {
-        // Get the system service for connectivity
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        // Check if the connectivity manager is not null
         if (connectivityManager != null) {
-            // Get the active network info
             NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-
-            // Return true if networkInfo is not null and the device is connected
             return networkInfo != null && networkInfo.isConnected();
         }
-
-        // Return false if connectivity manager is null
         return false;
     }
 }
